d50debf2d5c71c4a3f8d279733c5f9cd8577c2d4
MiyanoX <62541265+MiyanoX@users.noreply.github.com>
2023-02-15 03:42:26 +0800

    Menu CSS Updated

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,6 +1,6 @@
 import "./Menu.css";
 import { useDispatch, useSelector } from "react-redux";
-import { selectMenuDisplay } from "../searchBar/searchSlice";
+import { selectMenuDisplay, setMenuDisplay, selectScreenWidth } from "../searchBar/searchSlice";
 import { v4 as uuidv4 } from 'uuid';
 import { fetchRedditData } from "../cards/cardsSlice";
 
@@ -19,13 +19,20 @@ export const Menu = () => {
     const menuDisplay = useSelector(selectMenuDisplay);
     const menuList = initialMenuList;
     const dispatch = useDispatch();
+    const screenWidth= useSelector(selectScreenWidth);
+
 
     return (
         <div id="slider" className={`Menu ${menuAnimation(menuDisplay)}`} >
             <p id="menuTitle">Subreddit</p>
             <div id="menuList" >
                 {menuList.map((topic) => 
-                    <p id="topic" key={uuidv4()} onClick={() => {dispatch(fetchRedditData(topic))}} >/r/{topic}</p>
+                    <p id="topic" key={uuidv4()} onClick={() => {
+                        dispatch(fetchRedditData(topic)); 
+                        if (screenWidth <= 1000) {
+                            dispatch(setMenuDisplay());
+                        }
+                    }} >/r/{topic}</p>
                 )}
             </div>
             
